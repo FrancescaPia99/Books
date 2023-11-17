3f0128140c5149e377a240818c2886ecabb09283
FrancescaPia99 <148210261+FrancescaPia99@users.noreply.github.com>
2023-11-17 10:45:34 +0100

    browse list created

---
+++ Books/Views/GroupDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupDetailView.swift
+//  Books
+//
+//  Created by Francesca Pia De Rosa on 17/11/23.
+//
+
+import SwiftUI
+
+struct GroupDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupDetailView()
+}
